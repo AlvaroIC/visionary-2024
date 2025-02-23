36d86d2afe811d1a1360bec01f440932619f3f0b
Alejandro Ibáñez Coedo <alejandroibanezcoedo2009@gmail.com>
2025-02-23 18:06:22 +0100

    Detection of default user language.

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,14 +1,21 @@
 import { createI18n } from 'vue-i18n';
+import { supportedLocales } from './locales/supported-locales';
 
 import en from './locales/en.json';
 import es from './locales/es.json';
 import fr from './locales/fr.json';
 import de from './locales/de.json';
 
+// Get the user's language
+const userLocale = navigator.language;
+
+// Use the user's language if supported, otherwise fall back to 'en-US'
+const locale = supportedLocales.includes(userLocale) ? userLocale : 'en-US';
+
 // Create an i18n instance
 const i18n = createI18n({
   legacy: false, // Use Composition API
-  locale: 'en-US', // Default locale
+  locale, // Set the detected or fallback locale
   messages: {
     'en-US': en,
     'es-ES': es,
---
+++ src/locales/supported-locales.ts
@@ -0,0 +1,2 @@
+// List of supported locales
+export const supportedLocales = ['en-US', 'es-ES', 'fr-FR', 'de-DE'];
